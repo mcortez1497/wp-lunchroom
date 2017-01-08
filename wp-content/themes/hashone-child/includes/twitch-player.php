d5b9dd8052a3b8a17629fccb48a0973a546087f1
Michael Cortez <michael.cortez2@rackspace.com>
2017-01-07 21:55:55 -0600

    feat(twitch-player): add logic to handle no streams

--- wp-content/themes/hashone-child/includes/twitch-player.php
+++ wp-content/themes/hashone-child/includes/twitch-player.php
@@ -2,7 +2,6 @@
 
 class Twitch_Player {
 
-  // const TWITCH_API_URL = 'https://api.twitch.tv/kraken/search/streams?query=Magic:%20The%20Gathering';
   const TWITCH_API_URL = 'https://api.twitch.tv/kraken/streams/followed';
   const TWITCH_API_CLIENT_ID = '243x69xvxlj4lyk7juy2riu8ml30p4';
   const TWITCH_API_AUTH_TOKEN = 'gpnsx385cpzbw09jmt39ibo2arqiun';
@@ -20,25 +19,32 @@ class Twitch_Player {
 
     $html = "<div class='lr-twitch-wrapper'>"
           . "  <div class='lr-twitch-player'>";
-    if ( !wp_is_mobile() ) {
-      $html .= " <div class='lr-twitch-video'>"
-            .  "   <iframe src='http://player.twitch.tv/?channel=" . $streams[0]->channel->name . "' height='349' width='620'"
-            .  "            frameborder='0' scrolling='no' allowfullscreen='false'></iframe>"
+    if ( empty($streams) ) {
+      $html .= " <div class='lr-twitch-empty'>"
+            .  "   <h2>There are no streams going on right now</h2>"
+            .  "    <h5><a href='https://discordapp.com/invite/Eqmqe'>Join The Lunchroom</a> and start streaming to be shown here!</h5>"
             .  " </div>";
+    } else {
+      if ( !wp_is_mobile() ) {
+        $html .= " <div class='lr-twitch-video'>"
+              .  "   <iframe src='http://player.twitch.tv/?channel=" . $streams[0]->channel->name . "' height='349' width='620'"
+              .  "            frameborder='0' scrolling='no' allowfullscreen='false'></iframe>"
+              .  " </div>";
+      }
+      $html .= "   <div class='lr-channels'>";
+      foreach ($streams as $key=>$stream) {
+        $html .= "   <div class='lr-channel " . ($key == 0 && !wp_is_mobile() ? "active" : "") . "'>"
+              .  "     <img src=" .$stream->preview->medium . " alt='Stream' />"
+              .  "     <div class='lr-channel-description'>"
+              .  "       <div class='lr-channel-user'>" . $stream->channel->name . "</div>"
+              .  "       <div class='lr-channel-playing'>playing " . $stream->channel->game . "</div>"
+              .  "       <div class='lr-channel-status'>" . $stream->channel->status . "</div>"
+              .  "     </div>"
+              .  "   </div>";
+      }
+      $html .= "   </div>";
     }
-    $html .= "   <div class='lr-channels'>";
-    foreach ($streams as $key=>$stream) {
-      $html .= "   <div class='lr-channel " . ($key == 0 && !wp_is_mobile() ? "active" : "") . "'>"
-            .  "     <img src=" .$stream->preview->medium . " alt='Stream' />"
-            .  "     <div class='lr-channel-description'>"
-            .  "       <div class='lr-channel-user'>" . $stream->channel->name . "</div>"
-            .  "       <div class='lr-channel-playing'>playing " . $stream->channel->game . "</div>"
-            .  "       <div class='lr-channel-status'>" . $stream->channel->status . "</div>"
-            .  "     </div>"
-            .  "   </div>";
-    }
-    $html .= "   </div>"
-          .  " </div>"
+    $html .= " </div>"
           .  "</div>";
 
     return $html;
@@ -51,22 +57,20 @@ class Twitch_Player {
     $client_id = self::TWITCH_API_CLIENT_ID;
     $auth_token = self::TWITCH_API_AUTH_TOKEN;
 
-    $ch = curl_init($url);
 
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-      'Accept: application/vnd.twitchtv.v5+json',
-      'Client-ID: ' . $client_id,
-      'Authorization: OAuth ' . $auth_token
-    ));
+    $args = array(
+      'headers' => array(
+        "Accept" => "application/vnd.twitchtv.v5+json",
+        "Client-ID" => $client_id,
+        "Authorization" => "OAuth ".$auth_token
+      )
+    );
 
-    $data = curl_exec($ch);
+    $response = wp_remote_get($url, $args);
+    $response_body = wp_remote_retrieve_body($response);
 
-    curl_close($ch);
-
-    return json_decode($data);
+    return json_decode($response_body);
 
   }
-
 
 }
